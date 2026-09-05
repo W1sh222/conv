458d9b584dc35f6e9bad060cffca315eee21ac7e
W1sh222 <1400151410@sjtu.edu.com>
2026-09-05 07:14:26 +0000

    ample de .sh

--- eval/RULER/scripts/runq1.sh
+++ eval/RULER/scripts/runq1.sh
@@ -143,7 +143,7 @@ for MAX_SEQ_LENGTH in "${SEQ_LENGTHS[@]}"; do
         SETTINGS_INFO+="topk_${BLOCK_TOPK_RATIO##* }_"
     fi
     
-    RESULTS_DIR="${ROOT_DIR}/tf451_${SETTINGS_INFO}${MODEL_NAME}/${BENCHMARK}/${MAX_SEQ_LENGTH}"
+    RESULTS_DIR="${ROOT_DIR}/conv_kernel_7x7_qwen3_8b_ruler_mix_sparse_guarded_t065_multikey_qa2_48k64k_bf16_16000ema${SETTINGS_INFO}${MODEL_NAME}/${BENCHMARK}/${MAX_SEQ_LENGTH}"
     # RESULTS_DIR="${ROOT_DIR}/ruler_mix_guarded_t06_top16_1e4_step9500_topk07${SETTINGS_INFO}${MODEL_NAME}/${BENCHMARK}/${MAX_SEQ_LENGTH}"
     DATA_DIR="${RESULTS_DIR}/data"
     PRED_DIR="${RESULTS_DIR}/pred"
